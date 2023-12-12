330d464dc88a8593a4446ca415d37b2051c632c6
Rooni <Crispbaguette@gmail.com>
2023-12-12 18:49:29 +0100

    Initial commit

---
+++ LCBetterTerminal/BetterTerminal.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LCBetterTerminal.Patches;
+
+namespace LCBetterTerminal
+{
+
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class BetterTerminal : BaseUnityPlugin
+    {
+        private const string modGUID = "com.nekorooni.BetterTerminal";
+        private const string modName = "Better Terminal";
+        private const string modVersion = "0.0.1";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        public static BetterTerminal instance;
+
+        internal ManualLogSource log;
+
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+
+            log = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            log.LogInfo("Yo waddup");
+
+            harmony.PatchAll(typeof(BetterTerminal));
+            harmony.PatchAll(typeof(TerminalPatch));
+        }
+    }
+}
---
+++ LCBetterTerminal/Patches/TerminalPatch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+
+namespace LCBetterTerminal.Patches
+{
+    [HarmonyPatch(typeof(Terminal))]
+    internal class TerminalPatch
+    {
+        [HarmonyPatch("BeginUsingTerminal")]
+        [HarmonyPrefix]
+        public static void OpenTerminal(Terminal __instance)
+        {
+            BetterTerminal.instance.log.LogInfo("Begin terminal patch");
+            __instance.terminalUIScreen.gameObject.SetActive(true);
+        }
+
+        [HarmonyPatch("QuitTerminal")]
+        [HarmonyPrefix]
+        public static void CloseTerminal(Terminal __instance)
+        {
+            BetterTerminal.instance.log.LogInfo("Close terminal patch");
+            __instance.terminalUIScreen.gameObject.SetActive(false);
+        }
+    }
+}
